f5a6268764f8da57aba6b258e8160391f1c91256
Dmitry <dmitry@cojam.ru>
2020-12-28 01:18:14 +0500

    прокинул SCSS-переменные в конфиг next

--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
+const package = require('./package.json');
+
 const withPlugins = require('next-compose-plugins');
 
 const withImages = require('next-images');
 
-module.exports = withPlugins([withImages({ esModule: false, inlineImageLimit: false })]);
+module.exports = withPlugins([withImages({ esModule: false, inlineImageLimit: false })], {
+    sassOptions: {
+        // TODO: primeColor из конфига
+        additionalData: `
+            $VERSION: ${encodeURIComponent(package.version)};
+            $primeColor: #ad2929;
+        `,
+    },
+});
