ee44dbc07438e97f2074379801601d7d0ae78261
Dmitry <dmitry@cojam.ru>
2021-05-07 00:27:30 +0500

    убрал предупреждение

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,7 @@ import IndexPageContainer from '../src/Components/IndexPage/IndexPageContainer';
 import WarningMessage from '../src/Components/IndexPage/WarningMessage/WarningMessage';
 
 const IndexPage: React.FC = () => {
-    return (
-        <WarningMessage>
-            <IndexPageContainer />
-        </WarningMessage>
-    );
+    return <IndexPageContainer />;
 };
 
 export default IndexPage;
