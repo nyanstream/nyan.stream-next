846a9418fc1728f95e303c5b8c267d295c603b6c
Dmitry <git@e965.ru>
2021-12-02 19:33:13 +0500

    Update csp.ts

--- src/csp.ts
+++ src/csp.ts
@@ -1,9 +1,11 @@
+const UnsafeScriptAddon = process.env.NODE_ENV === 'development' ? `'unsafe-eval'` : '';
+
 const ContentSecurityPolicy =
     `default-src 'self';` +
     `child-src 'self' https://chatovod.ru https://thenyan.chatovod.ru https://wasd.tv https://player.twitch.tv https://goodgame.ru;` +
     // `frame-ancestors 'self';` +
     `style-src 'self' 'unsafe-inline' data: https://cdn.blyat.science https://fonts.googleapis.com; ` +
-    `script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdn.blyat.science;` +
+    `script-src 'self' 'unsafe-inline' ${UnsafeScriptAddon} https://cdn.blyat.science;` +
     `img-src 'self' data: https://cdn.blyat.science https://images.weserv.nl;` +
     `media-src 'self' blob:;` +
     `font-src 'self' https://fonts.gstatic.com;` +
