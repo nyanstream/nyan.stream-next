4264e5846d19a961d4ec916e84ea9a887d83fecd
Dmitry Dyachkov <git@e965.ru>
2023-12-31 16:32:04 +0500

    fix: security fix for new player (bruh)

--- src/config.ts
+++ src/config.ts
@@ -12,14 +12,12 @@ export const GoogleSiteVerification = 'CZKFsMeBuqFJ1KPYIaKptrBMmgolcM3bBbu6wt1Pf
 
 export const YandexVerification = 'eab231fe75b6da62';
 
-const UnsafeScriptAddon = process.env.NODE_ENV === 'development' ? `'unsafe-eval'` : '';
-
 export const ContentSecurityPolicy =
     `default-src 'self';` +
     `child-src 'self' https://chatovod.ru https://thenyan.chatovod.ru https://wasd.tv https://player.twitch.tv;` +
     // `frame-ancestors 'self';` +
     `style-src 'self' 'unsafe-inline' data: https://cdn.blyat.science https://fonts.googleapis.com; ` +
-    `script-src 'self' 'unsafe-inline' ${UnsafeScriptAddon} https://cdn.blyat.science;` +
+    `script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdn.blyat.science;` +
     `img-src 'self' data: https://cdn.blyat.science https://images.weserv.nl;` +
     `media-src 'self' blob:;` +
     `font-src 'self' https://fonts.gstatic.com;` +
