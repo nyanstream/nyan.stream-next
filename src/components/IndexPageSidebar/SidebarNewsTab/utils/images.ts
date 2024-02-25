e0378d2cbf1daa26ee1cf4c073f3a3ff45faf3e0
Dmitry Dyachkov <git@e965.ru>
2024-02-25 21:13:03 +0500

    chore: upgrade deps

--- src/components/IndexPageSidebar/SidebarNewsTab/utils/images.ts
+++ src/components/IndexPageSidebar/SidebarNewsTab/utils/images.ts
@@ -6,20 +6,18 @@ export const getVkImageSizes = (imageSrc: string | undefined) => {
 	const params = new URLSearchParams(new URL(imageSrc).search);
 	const sizeParam = params.get('size');
 
-	return (
-		(sizeParam
-			?.split('x')
-			.map(Number)
-			.map(value => value / 2) as [width: number, height: number]) || null
-	);
+	return sizeParam
+		?.split('x')
+		.map(Number)
+		.map(value => value / 2);
 };
 
 export const weservImageLoader = ({ src, width, quality }: ImageLoaderProps) => {
 	const params = new URLSearchParams({
-		w: width,
-		q: quality || 75,
+		w: String(width),
+		q: String(quality || 75),
 		url: src,
-	} as any);
+	});
 
 	return `https://images.weserv.nl/?${params.toString()}`;
 };
