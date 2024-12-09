3324b3ba45cf24c92fe5378dc1869e08ea8cb947
Dmitry Dyachkov <git@e965.ru>
2024-12-09 10:57:26 +0500

    feat: leave only chat tab in sidebar

--- src/components/IndexPageSidebar/SidebarContainer.tsx
+++ src/components/IndexPageSidebar/SidebarContainer.tsx
@@ -1,15 +1,8 @@
-import { useState, useCallback } from 'react';
-
 import type { ReactComponent } from '@/types';
-
-import type { SidebarTabKey } from './SidebarTypes';
 
 import { useTheme } from '@/hooks';
 
-import { SidebarRadioButtons } from './SidebarRadioButtons/SidebarRadioButtons';
 import { SidebarChatTab } from './SidebarChatTab/SidebarChatTab';
-import { SidebarScheduleTab } from './SidebarScheduleTab/SidebarScheduleTab';
-import { SidebarNewsTab } from './SidebarNewsTab/SidebarNewsTab';
 
 import styles from './Sidebar.module.scss';
 
@@ -22,31 +15,10 @@ export const SidebarContainer: ReactComponent<PropsType> = props => {
 
 	const { Theme } = useTheme();
 
-	const [CurrentSidebarTab, setCurrentSidebarTab] = useState<SidebarTabKey>('chat');
-
-	const handleSidebarRadioButtonClick = useCallback((tabkey: SidebarTabKey) => {
-		setCurrentSidebarTab(tabkey);
-	}, []);
-
 	return (
 		<aside className={styles.sidebar} data-theme={Theme} hidden={IsSidebarHidden}>
-			<SidebarRadioButtons {...{ CurrentSidebarTab }} {...{ handleSidebarRadioButtonClick }} />
-
 			<div className={styles.sidebar__tabs}>
-				<SidebarChatTab
-					className={styles.sidebar__tabs__tab}
-					isVisible={CurrentSidebarTab === 'chat'}
-				/>
-
-				<SidebarScheduleTab
-					className={styles.sidebar__tabs__tab}
-					isVisible={CurrentSidebarTab === 'schedule'}
-				/>
-
-				<SidebarNewsTab
-					className={styles.sidebar__tabs__tab}
-					isVisible={CurrentSidebarTab === 'news'}
-				/>
+				<SidebarChatTab className={styles.sidebar__tabs__tab} isVisible={true} />
 			</div>
 		</aside>
 	);
